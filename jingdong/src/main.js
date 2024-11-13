19a3bd5d7439c9ff0aa7bf2b89d40449752f7c9f
Jack <igitinode@gmail.com>
2024-11-13 16:05:43 +0800

    feat(deps): 8.3 rem 基础样式 normalize.css

--- jingdong/src/main.js
+++ jingdong/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 为了让样式在所有的浏览器都保持一致
+// 让normalize.css帮我们不同浏览器之间显示的差异
+import 'normalize.css'
+import './style/base.scss'
 
 createApp(App).use(store).use(router).mount('#app')
