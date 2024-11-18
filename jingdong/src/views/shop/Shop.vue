2fff11c4eb90ea98200fc748ef1c064001a7433b
Jack <igitinode@gmail.com>
2024-11-18 10:53:45 +0800

    feat(components): 10.4 搜索布局及路由跳转1

--- jingdong/src/views/shop/Shop.vue
+++ jingdong/src/views/shop/Shop.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="wrapper">
+    <div class="search">
+      <div class="search-back iconfont">&#xe602;</div>
+      <div class="search-content">
+        <span class="search-content-icon"></span>
+        <input class="search-content-input" />
+      </div>
+    </div>
     <shop-info :item="item" :hideBorder="true" />
   </div>
 </template>
@@ -34,5 +41,39 @@ export default {
 @import '@/style/viriables.scss';
 .wrapper {
   padding: 0 0.18rem;
+}
+.search {
+  display: flex;
+  margin: 0.2rem 0 0.16rem 0;
+  line-height: 0.32rem;
+
+  &-back {
+    width: 0.3rem;
+    font-size: 0.24rem;
+    color: #b6b6b6;
+  }
+
+  &-content {
+    display: flex;
+    flex: 1;
+    background-color: #f5f5f5;
+    border-radius: 0.16rem;
+
+    &-icon {
+      width: 0.44rem;
+      height: 0.32rem;
+      background: blue;
+    }
+
+    &-input {
+      display: block;
+      width: 100%;
+      padding-right: 0.2rem;
+      border: none;
+      outline: none;
+      background: none;
+      height: 0.32rem;
+    }
+  }
 }
 </style>
