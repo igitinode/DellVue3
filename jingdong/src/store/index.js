15959ab2d83f7452b02a4716a5b44d0e85dc0f5a
Jack <igitinode@gmail.com>
2024-11-20 10:57:57 +0800

    feat(components): 11.3 Vuex中购物车数据结构的设计2

--- jingdong/src/store/index.js
+++ jingdong/src/store/index.js
@@ -23,6 +23,21 @@ export default createStore({
   mutations: {
     addItemToCart(state, payload) {
       const { shopId, productId, productInfo } = payload
+      let shopInfo = state.cartList[shopId]
+      if (!shopInfo) {
+        shopInfo = {}
+      }
+
+      let product = shopInfo[productId]
+      if (!product) {
+        product = productInfo
+        product.count = 0
+      }
+
+      product.count += 1
+      shopInfo[productId] = product
+
+      state.cartList[shopId] = shopInfo
     }
   },
   actions: {},
