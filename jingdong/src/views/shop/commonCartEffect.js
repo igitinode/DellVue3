e6052e43392db5004e69934ba0d9f02171685053
igitinode <igitinode@gmail.com>
2024-11-21 20:20:32 +0800

    feat(components): 11.14 购物车修复

--- jingdong/src/views/shop/commonCartEffect.js
+++ jingdong/src/views/shop/commonCartEffect.js
@@ -1,10 +1,10 @@
-import { ref, toRefs } from 'vue'
+import { ref } from 'vue'
 import { useStore } from 'vuex'
 
 // 购物车相关逻辑
 export const useCommonCartEffect = () => {
   const store = useStore()
-  const { cartList } = toRefs(store.state)
+  const cartList = store.state.cartList
   const changeCartItemInfo = (shopId, productId, productInfo, num) => {
     store.commit('changeCartItemInfo', { shopId, productId, productInfo, num })
   }
